5531eeb41fe726160711fba732a74b5784572d47
sjahaf <salj4215@google.com>
2017-11-27 01:51:57 -0500

    added comment

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require ('header.phtml');
 
 if (isset($_GET['page']) && $_GET['page'] == 'aboutme')
     require ('aboutme.phtml');
+elseif (isset($_GET['page']) && $_GET['page'] == 'projects')
+    require ('Projects.phtml');
 else
     require ('home.phtml');
 
