998d8daee06f6ea90efd00ca1d3f4e0ddda88ec5
Salleh Jahaf <salj4215@gmail.com>
2018-03-18 23:00:03 -0400

    added svg testing file added svg testing file link to nav bar added php code to index file for svg testing page

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ if (isset($_GET['page']) && $_GET['page'] == 'aboutme')
     require ('aboutme.phtml');
 elseif (isset($_GET['page']) && $_GET['page'] == 'projects')
     require ('Projects.phtml');
+elseif (isset($_GET['page']) && $_GET['page'] === 'svgtesting')
+    require ('svgtesting.phtml');
 else
     require ('home.phtml');
 
