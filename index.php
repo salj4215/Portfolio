2a0715037a3bd5b908291b34e27c1c8f71e4197c
Salleh Jahaf <salj4215@gmail.com>
2019-07-25 02:03:45 -0400

    removing svg elements

--- index.php
+++ index.php
@@ -6,8 +6,6 @@ if (isset($_GET['page']) && $_GET['page'] == 'aboutme')
     require ('aboutme.phtml');
 elseif (isset($_GET['page']) && $_GET['page'] == 'projects')
     require ('Projects.phtml');
-elseif (isset($_GET['page']) && $_GET['page'] === 'svgtesting')
-    require ('svgtesting.phtml');
 else
     require ('home.phtml');
 
